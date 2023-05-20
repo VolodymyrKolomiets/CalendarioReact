231bc037e2ad380d5644c36266c35d2710a2e7e2
adrianMartinezTeba <adrymate@gmail.com>
2023-05-20 19:34:43 +0200

    haciendo el footer funcional de navegacion

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './Footer.scss';
-
+import { Link } from "react-router-dom";
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div>
+      <Link to={'/'}>Home</Link>
+      <Link to={'/matches'}>Matches</Link>
+      <Link to={'/chat'}>Chat</Link>
+      <Link to={'/calendar'}>Calendar</Link>
+      <Link to={'/profile'}>Profile</Link>
+      </div>
   )
 }
 
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,64 @@
-import React from 'react'
-import './Login.scss';
-
-const Login = () => {
-  return (
-    <div>Login</div>
-  )
+import React, { useContext, useState,useEffect} from "react";
+import { UserContext } from "../../context/UserContext/UserState";
+export const Login= () => {
+  const {login,token} = useContext(UserContext)
+  const [data, setData] = useState({
+email: "",
+password: "",
+});
+const initialState ={
+  email:"",
+  password:"",
 }
+const clearState = () => {
 
-export default Login
+  setData({ ...initialState });
+  
+  };
+const handleInputChange = (event) => {
+  setData({
+    ...data,
+    [event.target.name]: event.target.value,
+  });
+  
+  };
+  useEffect(() => {
+    if (token) {
+      navigate("/");
+      console.log('usuario conectado con exito');
+    }else{
+      console.log('Ha habido un error al iniciar sesion');
+    }
+  }, [token]);
+  
+  const handleSubmit = (values) => {
+ 
+  login(values)
+    console.log("sending data..." );
+    clearState();
+    };
+    return (
+      <div>
+        
+        <form onSubmit={handleSubmit}>
+        <label htmlFor="password">Email:</label>
+          <input
+            type="email"
+            placeholder="email"
+            value={data.email} 
+            onChange={handleInputChange}
+            name="email"
+          />
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            placeholder="password"
+            value={data.password}
+            onChange={handleInputChange}
+            name="password"
+          />
+          <button type="submit">Enviar</button>
+        </form>
+      </div>
+    );
+    }
