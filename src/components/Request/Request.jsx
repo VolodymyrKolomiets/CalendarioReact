e93d98e56a34133d18ad76d5100855274ecfe079
VolodymyrKolomiets <kolomiietsv88@gmail.com>
2023-05-21 16:11:06 +0200

    add request(peticiones)

--- src/components/Request/Request.jsx
+++ src/components/Request/Request.jsx
@@ -1,20 +1,98 @@
-import "./Request.scss"
+import React, { useEffect, useState } from 'react';
+import './Request.scss';
+import { DownOutlined, ClockCircleFilled } from '@ant-design/icons'
+import axios from 'axios';
+import rachel from "../../assets/icons/rachel.png"
 
-import React from 'react'
+const userPhoto = rachel;
+const eventName = 'Sesión fotográfica paramoda y editorial';
+const formattedDate = '01/01/2022';
+const eventPhase = 'Phase 1';
+const chatButton = 'Chat';
 
 const Request = () => {
+
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+    const [requests, setRequests] = useState(() => {
+        const localData = localStorage.getItem('requests');
+        return localData ? JSON.parse(localData) : [];
+    });
+
+    const toggleDropdown = () => {
+        setIsDropdownOpen(!isDropdownOpen);
+    };
+
+    const saveLocalRequests = (requests) => {
+        localStorage.setItem('requests', JSON.stringify(requests));
+    };
+
+    const getRequests = async () => {
+        try {
+            const req = await axios.get("la ruta GET de las peticiones con los matches en un concreto evento ")
+            // console.log(req)
+            setRequests(req.data.results) //Request con req.data.ect hasta llegar a la informacion de las peticiones
+            saveLocalRequests(req.data.results);//Guardando el estado de request en el localstorege
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getRequests()
+    }, [])
+
     return (
-        <div>
-            <div>
-                <h1 className="request-header">
-                    Peticiones
-                </h1>
+        <>
+            <div className="footer-principal-container">
+                <div className="footer-container">
+                <div className="footer-bar">footer</div>
+                </div>
             </div>
-            <div>
+            <div className="principal-container">
+                <div className="secundary-container">
+                    <div className="text-container">
+                        <h1 className="request-text">Peticiones</h1>
+                    </div>
+                    <div className="request-line"></div>
+                </div>
+                <div className={"principal-dropdown-container " + (isDropdownOpen ? "open" : "")} onClick={toggleDropdown}>
+                    <div className="dropdown-container">
+                        <div className="clock-container">
+                            <div className="clock"><ClockCircleFilled className="gris" /></div>
+                        </div>
+                        <div className="dropdown-text">
+                            <div>Martes , 12 Mayo</div>{/* un map. para sacar la info de los dias */}
+                            {isDropdownOpen && (
+                                <div className="dropdown-content">
+                                    <div className="user-container"> {/* un map. para sacar la informacion de la peticion "match" recibido */}
+                                        <img className="avatar" src={userPhoto} alt="User" />
+                                        <div className="info-request"> {/* //=text */}
+                                            <p className="titel-request">{eventName}</p>
+                                            <p className="description-request">{eventPhase} | {formattedDate}</p>
+                                        </div>
+                                        {chatButton && <button className='button-chat'>{chatButton}</button>}
+                                        {/*       {requests.map(request => {
+                                    return (
+                                        <div key={request.id}>
+                                            <img src={request.img} alt="User" />
+                                            <h3>{request.name}</h3>
+                                            <p>{request.eventPhase} | {request.formattedDate}</p>
+                                            {chatButton && <button>{chatButton}</button>}
+                                        </div>
+                                    )
+                                })} */}
+                                    </div>
+                                </div>
+                            )}
+                        </div>
+                        <div className="drop-arrow"><DownOutlined className="gris" /></div>
+                    </div>
 
+                </div>
             </div>
-        </div>
-    )
-}
+        </>
+    );
+};
 
-export default Request
+export default Request;
